0fe28a8f8d50d174be2d5a7e28d54a3836438d5f
unknown <mjawwadiqbal22@gmail.com>
2019-04-02 19:31:13 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const mysql=require('mysql');
+const express=require('express');
+var app =express();
+
+const bodyParser=require('body-parser');
+
+app.use(bodyParser.json());
+
+
+var mysqlConnection=mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'EmployeeDB',
+});
+
+mysqlConnection.connect((err)=>{
+    if(!err){
+        console.log('DB Connection success')
+    }
+    else{
+        console.log('Failed')
+    }
+});
+
+app.listen(3000,()=>console.log('running on 3000'));
+
+app.get('/employees',(req,res)=>{
+    mysqlConnection.query('SELECT * FROM Employee',(err,rows,fields)=>{
+        if(!err){
+            res.send(rows)
+            console.log(rows);
+            
+        }
+        else{
+            console.log(err);
+        }
+    })
+})
